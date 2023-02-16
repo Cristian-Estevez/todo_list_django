46dbf036b7447a4a8c2f0489ae3d812fbbe9e99c
Cristian-Estevez <estevezcristian@gmail.com>
2023-02-16 08:35:02 -0300

    Se agregan mas iconos al navbar y a la lista de tareas

--- src/static/main/main.js
+++ src/static/main/main.js
@@ -1,15 +1,20 @@
+$(document).ready(function () {
+  $('.checkbox-blank').on('click', function (event) {
+    $(event.target).html = 'check_box'  aca revisar funcion
+  })
+})
 
 function toggleTheme() {
   let currentTheme = document.getElementById('body').classList[0];
-  if (currentTheme == "light") {
+  if (currentTheme == 'light') {
     document.getElementById('body').classList.remove('light');
     document.getElementById('body').classList.add('dark');
-    $('#theme-toggler-text').html('Light mode');
     $('#theme-toggler-icon').html('light_mode');
+    document.getElementById('theme-toggler-icon').title = 'Light mode';
   } else {
     document.getElementById('body').classList.remove('dark');
     document.getElementById('body').classList.add('light');
-    $('#theme-toggler-text').html('Dark mode');
     $('#theme-toggler-icon').html('dark_mode');
+    document.getElementById('theme-toggler-icon').title = 'Dark mode';
   }
 }
