9284286f28eef116f62dd60193469b03eecf30bd
cristian <estevezcristian@gmail.com>
2023-02-21 22:36:50 -0300

    front-end visual improvements

--- src/static/main/main.js
+++ src/static/main/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+  //checkbox click event
   $('.checkbox-blank').on('click', function (event) {
+    event.stopPropagation();
     let iconText = event.target.innerHTML;
     if (iconText === 'check_box_outline_blank') {
       event.target.innerHTML = 'check_box';
@@ -8,7 +10,32 @@ $(document).ready(function () {
       event.target.innerHTML = 'check_box_outline_blank';
       event.target.classList.remove('task-done')
     }
-  })
+  });
+
+  // hover effects
+  $('.folder').hover(function (event) {
+    $(event.currentTarget).find(".folder-container").css('background-color', 'rgb(0, 0, 0, .15)');
+  }, function (event) {
+    $(event.currentTarget).find(".folder-container").css('background-color', '');
+  });
+
+  $('.folder').on('click', function () {
+    if ($(this).find('.folder-tasks-container').attr('class').includes('display-none')) {
+      $(this).find('.folder-tasks-container').removeClass('display-none');
+      $(this).css('background-color', 'rgb(0, 0, 0, .15)');
+      $(this).find(".folder-container .featured-icon-container .list-icon").text('folder_open');
+    } else {
+      $(this).find('.folder-tasks-container').addClass('display-none')
+      $(this).css('background-color', '')
+      $(this).find(".folder-container .featured-icon-container .list-icon").text('folder');
+    };
+  });
+
+  $('.task').hover(function (event) {
+    $(event.currentTarget).css('background-color', 'rgb(0, 0, 0, .15)');
+  }, function (event) {
+    $(event.currentTarget).css('background-color', '');
+  });
 })
 
 function toggleTheme() {
@@ -25,3 +52,4 @@ function toggleTheme() {
     document.getElementById('theme-toggler-icon').title = 'Dark mode';
   }
 }
+
--- src/todo_list/models.py
+++ src/todo_list/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Folder(models.Model):
+    user_id = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    deleted = models.BooleanField(default="false")
+
+
+class Task(models.Model):
+    user_id = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=600)
+    deleted = models.BooleanField(default="false")
+    folder_id = models.ForeignKey(
+        to=Folder, null="true", on_delete=models.CASCADE
+    )
